2cd6fdcf7b2d18e9c15293350b217df00f108b21
Adnia Salsabila <adniasalsabila8@gmail.com>
2021-09-18 19:52:28 +0700

    Commit Register

---
+++ app/src/main/java/com/nurapriyanto/miniprojekglints_2_absensi/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.nurapriyanto.miniprojekglints_2_absensi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nurapriyanto.miniprojekglints_2_absensi.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
